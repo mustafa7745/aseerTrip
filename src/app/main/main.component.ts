c3e13d61e00cf07db5f8a0d88adfac9579b9ebeb
musta <mustafa774519161@gmail.com>
2025-04-27 23:43:25 +0300

    v7

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -130,21 +130,25 @@ export class MainComponent {
 
   featuredGuides = [
     {
-      name: 'أحمد السلمي',
+      id: 1,
+      name:  'مهند سعيد',
       avatar:
-        'https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg',
+        'https://bypzmbcssqynwcwglaan.supabase.co/storage/v1/object/public/tours/guider/mohaned.png',
       languages: ['العربية', 'الإنجليزية'],
       rating: 4.8,
     },
     {
-      name: 'محمد القحطاني',
+      id: 2,
+
+      name: 'علي سعد',
       avatar:
-        'https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg',
+        'https://bypzmbcssqynwcwglaan.supabase.co/storage/v1/object/public/tours/guider/saad.png',
       languages: ['العربية', 'الفرنسية'],
       rating: 4.9,
     },
     {
-      name: 'مهند أبو عجمه',
+      id: 3,
+      name: 'محمد أبو عجمه',
       avatar:
         'https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg',
       languages: ['العربية', 'الفرنسية'],
